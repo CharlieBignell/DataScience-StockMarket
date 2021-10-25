308c6cb70410ff7ea4b305532bc73a4ad9660a59
Charlie <charlie_jb@hotmail.co.uk>
2021-10-25 16:18:46 +0100

    split other transactions and separated dividend

--- cleanup.py
+++ cleanup.py
@@ -12,9 +12,9 @@ df_tran = pd.read_csv('transactions_raw.csv')
 df_tran.rename(columns={'Value Date': 'Date'}, inplace=True)
 
 
-
 def removeShareCount(string):
     return " ".join(string.split()[1:])
+
 
 def format(df):
     # Use regular expressions to extract the different components of from the 'details' column
@@ -29,15 +29,16 @@ def format(df):
     df["Currency"] = df["FXRate"].str.extract(r'([A-Z]+(?=\)))')
     df["FXRate"] = df["FXRate"].str.extract(r'(= \d+.\d+)')
     df["FXRate"] = df["FXRate"].str[2:]
-    df.loc[df["FXRate"].isnull(),'FXRate'] = "1"
-    df.loc[df["Currency"].isnull(),'Currency'] = "GBP"
+    df.loc[df["FXRate"].isnull(), 'FXRate'] = "1"
+    df.loc[df["Currency"].isnull(), 'Currency'] = "GBP"
 
     # Now that we've extracted all we need, drop the 'Details'  and 'Type' columns
     df.drop(columns=["Details", "Type"], inplace=True)
 
     # Convert numeric columns
-    df[["ShareCount", "Value", "FXRate"]] = df[["ShareCount", "Value", "FXRate"]].apply(pd.to_numeric)
-    
+    df[["ShareCount", "Value", "FXRate"]] = df[[
+        "ShareCount", "Value", "FXRate"]].apply(pd.to_numeric)
+
     df = df.reset_index(drop=True)
 
 
@@ -56,25 +57,30 @@ format(df_sells)
 df_deposit = df_tran.loc[df_tran["Type"] == "DEPOSIT", ["Date", "Value"]]
 df_deposit = df_deposit.reset_index(drop=True)
 
-
-######################
-#   OTHER ACTIVITY   #
-######################
+# Get all dividends in a single dataframe
+df_dividend = df_tran.loc[df_tran["Type"] == "INVESTMENT_INCOME", ["Date", "Details", "Value"]]
+df_dividend["ISIN"] = df_dividend["Details"].str.extract(r'([A-Z]{2}[A-Z0-9]{9}[0-9]{1})')
+# df_dividend["Name"] = df_dividend["Details"].str.extract(r'(.+(?= \([A-Z]{2}[A-Z0-9]{9}[0-9]{1}))')
 
 # Put the remaning activity in a separate dataframe. The value of each transaction here tends to be very small
-df_other = df_tran.loc[
-    (df_tran["Type"] == "INTEREST") | 
-    (df_tran["Type"] == "INVESTMENT_INCOME") | 
-    (df_tran["Type"] == "FX_FEE") |
-    (df_tran["Type"] == "STAMP_DUTY"), 
-    ["Date", "Type", "Details", "Value"]]
+df_interest = df_tran.loc[df_tran["Type"] == "INTEREST", ["Date", "Value"]]
+df_fx = df_tran.loc[df_tran["Type"] == "FX_FEE", ["Date", "Type", "Value"]]
+df_stamp = df_tran.loc[df_tran["Type"] == "STAMP_DUTY", ["Date", "Type", "Value"]]
+
+df_stamp["Type"] = "Stamp Duty"
+df_interest["Type"] = "Interest"
+df_fx["Type"] = "FX Fee"
+
+df_other = pd.concat([df_stamp, df_interest, df_fx])
 df_other = df_other.reset_index(drop=True)
 
 df_buys.to_csv('buys.csv', index=False)
 df_sells.to_csv('sells.csv', index=False)
 df_deposit.to_csv('deposits.csv', index=False)
+df_dividend.to_csv('dividend.csv', index=False)
+df_other.to_csv('other.csv', index=False)
 
 # Make sure all rows have been included in the new dataframes
-errorFlag = len(df_tran.index) - len(df_deposit.index) - len(df_buys.index) - len(df_sells.index) - len(df_other.index)
+errorFlag = len(df_tran.index) - len(df_deposit.index) - len(df_buys.index) - len(df_sells.index) - len(df_dividend.index) - len(df_other.index)
 if errorFlag != 0:
     print("ERROR: " + str(errorFlag) + " row(s) missing in new dataframes")
