bc0aa5fac817d836a2fee1d5dc972d1be21b8008
Charlie <charlie_jb@hotmail.co.uk>
2021-10-26 18:36:39 +0100

    Moved daily generation to analyse

--- analyse.py
+++ analyse.py
@@ -1,17 +1,17 @@
 import pandas as pd
+import numpy as np
 
-df_buys = pd.read_csv('buys.csv')
-df_sells = pd.read_csv('sells.csv')
-df_deposits = pd.read_csv('deposits.csv')
+# Get the data
+df_basic = pd.read_csv('basic.csv')
 df_other = pd.read_csv('other.csv')
 df_dividend = pd.read_csv('dividend.csv')
 
+# Generate a daily summary table from the 'other' dataframe
+df_dailySummary = pd.pivot_table(df_other, values="Value", index="Date", columns="Type", aggfunc=np.sum).fillna(0)
+df_dailySummary[["FX Fee", "Deposit", "Interest", "Stamp Duty"]] = df_dailySummary[["FX Fee", "Deposit", "Interest", "Stamp Duty"]].round(2)
 
-# Portfolio summary
-totalSpent = df_buys.groupby('Name').agg({'Value': 'sum'})
-sharesIn = df_buys.groupby('Name').agg({'ShareCount': 'sum'})['ShareCount']
-sharesOut = df_sells.groupby('Name').agg({'ShareCount': 'sum'})['ShareCount']
-netShares = (sharesIn - sharesOut).round(2)
+# # Add bought to daily
+# df_buys = df_basic.loc[df_basic["Type"] == "BUY", ["Date", "Value"]]
+# df_sells = df_basic.loc[df_basic["Type"] == "SELL", ["Date", "Value"]]
 
-print(netShares)
-
+# print(df_sells)
--- cleanup.py
+++ cleanup.py
@@ -7,58 +7,56 @@ import numpy as np
 # Details: text explanation of transaction e.g. '1.5 Tesla Motors Inc (US88160R1014) @ USD 615.10000 (@ 1 USD = 1.38887 GBP) on 04-Mar-2021 20:30:26'
 # Value: the amount bought/sold/given in GBP. It's signed i.e. outgoings are negative
 
-# Read data and rename date column
+# Read data and format the date column
 df_tran = pd.read_csv('transactions_raw.csv')
 df_tran.rename(columns={'Value Date': 'Date'}, inplace=True)
 df_tran['Date'] = pd.to_datetime(df_tran['Date'], format="%d/%m/%Y")
 
+# Read the data on stock splits and format the date column
 df_splits = pd.read_csv('splits.csv')
 df_splits['Date'] = pd.to_datetime(df_splits['Date'], format="%d/%m/%Y")
 
+#########
+# BASIC #
+#########
 
+# Get all buys and sells in a single dataframe
+df_basic = df_tran.loc[(df_tran["Type"] == "BUY") | (df_tran["Type"] == "SELL"), ["Date", "Type", "Details", "Value"]]
+
+# Use regular expressions to extract the different components of from the 'details' column
+df_basic["ShareCount"] = df_basic["Details"].str.extract(r'(^\d+[.]\d+|^\d+[^\\])')
+df_basic["FXRate"] = df_basic["Details"].str.extract(r'(\(@.+?\))')
+df_basic["ISIN"] = df_basic["Details"].str.extract(r'([A-Z]{2}[A-Z0-9]{9}[0-9]{1})')
+df_basic["Name"] = df_basic["Details"].str.extract(r'(.+(?= \([A-Z]{2}[A-Z0-9]{9}[0-9]{1}))')
+
+# 'name' still contains the share count which we need to remove
 def removeShareCount(string):
     return " ".join(string.split()[1:])
+df_basic["Name"] = df_basic["Name"].apply(removeShareCount)
 
+df_basic["Currency"] = df_basic["FXRate"].str.extract(r'([A-Z]+(?=\)))')
+df_basic["FXRate"] = df_basic["FXRate"].str.extract(r'(= \d+.\d+)')
+df_basic["FXRate"] = df_basic["FXRate"].str[2:]
+df_basic.loc[df_basic["FXRate"].isnull(), 'FXRate'] = "1"
+df_basic.loc[df_basic["Currency"].isnull(), 'Currency'] = "GBP"
 
-def format(df):
-    # Use regular expressions to extract the different components of from the 'details' column
-    df["ShareCount"] = df["Details"].str.extract(r'(^\d+[.]\d+|^\d+[^\\])')
-    df["FXRate"] = df["Details"].str.extract(r'(\(@.+?\))')
-    df["ISIN"] = df["Details"].str.extract(r'([A-Z]{2}[A-Z0-9]{9}[0-9]{1})')
-    df["Name"] = df["Details"].str.extract(r'(.+(?= \([A-Z]{2}[A-Z0-9]{9}[0-9]{1}))')
+# Now that we've extracted all we need, drop the 'Details' column
+df_basic.drop(columns=["Details"], inplace=True)
 
-    # 'name' still contains the share count which we need to remove
-    df["Name"] = df["Name"].apply(removeShareCount)
-
-    df["Currency"] = df["FXRate"].str.extract(r'([A-Z]+(?=\)))')
-    df["FXRate"] = df["FXRate"].str.extract(r'(= \d+.\d+)')
-    df["FXRate"] = df["FXRate"].str[2:]
-    df.loc[df["FXRate"].isnull(), 'FXRate'] = "1"
-    df.loc[df["Currency"].isnull(), 'Currency'] = "GBP"
-
-    # Now that we've extracted all we need, drop the 'Details'  and 'Type' columns
-    df.drop(columns=["Details", "Type"], inplace=True)
-
-    # Convert numeric columns
-    df[["ShareCount", "Value", "FXRate"]] = df[[
-        "ShareCount", "Value", "FXRate"]].apply(pd.to_numeric)
-
-
-# Get all buys in a single dataframe, and format
-df_buys = df_tran.loc[df_tran["Type"] == "BUY", ["Date", "Type", "Details", "Value"]]
-format(df_buys)
+# Convert numeric columns
+df_basic[["ShareCount", "Value", "FXRate"]] = df_basic[["ShareCount", "Value", "FXRate"]].apply(pd.to_numeric)
 
 # Alter share count of certain stocks to account for stock splits
 for index, row in df_splits.iterrows():
-    df_buys.loc[(df_buys["Name"] == row["Name"]) & (df_buys["Date"] < row["Date"]), ["ShareCount"]] = (df_buys['ShareCount'] * row["Split"]).round(6)
+    df_basic.loc[(df_basic["Name"] == row["Name"]) & (df_basic["Date"] < row["Date"]), ["ShareCount"]] = (df_basic['ShareCount'] * row["Split"]).round(6)
 
 
-# Convert the value to positive i.e. amount spent, rather than net balance, as this is more intuitive now that we've separated buying and selling
-df_buys["Value"] = df_buys["Value"]*-1
+# Convert buy values to positive
+df_basic.loc[df_basic["Type"] == "BUY", "Value"] = df_basic["Value"]*-1
 
-# Get all sells in a single dataframe
-df_sells = df_tran.loc[df_tran["Type"] == "SELL", ["Date", "Type", "Details", "Value"]]
-format(df_sells)
+#############
+# Dividends #
+#############
 
 # Get all dividends in a single dataframe
 df_dividend = df_tran.loc[df_tran["Type"] == "INVESTMENT_INCOME", ["Date", "Details", "Value"]]
@@ -66,6 +64,10 @@ df_dividend["ISIN"] = df_dividend["Details"].str.extract(r'([A-Z]{2}[A-Z0-9]{9}[
 df_dividend["Name"] = df_dividend["Details"].str.extract(r'(m .+(?= \([A-Z]{2}[A-Z0-9]{9}[0-9]{1}))')
 df_dividend["Name"] = df_dividend["Name"].apply(removeShareCount)
 df_dividend.drop(columns=["Details"], inplace=True)
+
+#########
+# Other #
+#########
 
 # Put the remaning activity in a separate dataframe. The value of each transaction here tends to be very small
 df_interest = df_tran.loc[df_tran["Type"] == "INTEREST", ["Date", "Value"]]
@@ -78,14 +80,10 @@ df_interest["Type"] = "Interest"
 df_fx["Type"] = "FX Fee"
 df_deposit["Type"] = "Deposit"
 
-df_dailySummary = pd.concat([df_stamp, df_interest, df_fx, df_deposit])
-df_dailySummary = df_dailySummary.reset_index(drop=True)
-df_dailySummary = pd.pivot_table(df_dailySummary, values="Value", index="Date", columns="Type", aggfunc=np.sum).fillna(0)
-df_dailySummary[["FX Fee", "Deposit", "Interest", "Stamp Duty"]] = df_dailySummary[["FX Fee", "Deposit", "Interest", "Stamp Duty"]].round(2)
+df_other = pd.concat([df_stamp, df_interest, df_fx, df_deposit])
+df_other = df_other.reset_index(drop=True)
 
-# Add buys, sells, and cash value to daily
-
-df_buys.to_csv('buys.csv', index=False)
-df_sells.to_csv('sells.csv', index=False)
+# Output dataframes to CSV
+df_basic.to_csv('basic.csv', index=False)
 df_dividend.to_csv('dividend.csv', index=False)
-df_dailySummary.to_csv('daily.csv', index=True)
+df_other.to_csv('other.csv', index=True)
