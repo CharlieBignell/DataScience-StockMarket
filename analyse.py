2ca840670a5aebb0ae1fd3c3ceea0099e66d88c5
Charlie <charlie_jb@hotmail.co.uk>
2021-10-27 20:59:08 +0100

    Started getting portfolio value on each day

--- analyse.py
+++ analyse.py
@@ -6,40 +6,17 @@ import yfinance as yf
 df_basic = pd.read_csv('./cleaned/basic.csv')
 df_other = pd.read_csv('./cleaned/other.csv')
 df_dividend = pd.read_csv('./cleaned/dividend.csv')
+
+# Format date fields
 df_basic['Date'] = pd.to_datetime(df_basic['Date'])
 df_other['Date'] = pd.to_datetime(df_other['Date'])
 df_dividend['Date'] = pd.to_datetime(df_dividend['Date'])
 
-# Get a list of all tickers traded
-tickers_all = df_basic['Name'].unique()
+#############
+# Functions #
+#############
 
-
-# Account for any stock splits
-allSplits = []
-for ticker in tickers_all:
-
-    # Get the split and convert to a df
-    splits = yf.Ticker(ticker).splits
-    df_split = pd.DataFrame({'Date':splits.index, 'Split':splits.values})
-    df_split["Name"] = ticker
-
-    # If there was a split, append the df to allSplits. The ignoring of PFE is a quirk with a spin off they did with Viatris in 2020. This is a bad fix but it'll do for now
-    if len(df_split.index) != 0 and ticker != "PFE":
-        allSplits.append(df_split)
-
-allSplits = pd.concat(allSplits)
-
-for index, row in allSplits.iterrows():
-    df_basic.loc[(df_basic["Name"] == row["Name"]) & (df_basic["Date"] < row["Date"]), ["ShareCount"]] = (df_basic['ShareCount'] * row["Split"]).round(6)
-
-
-#Split buys and sells into separate dataframes
-df_buys = df_basic.loc[df_basic["Type"] == "BUY"]
-df_sells = df_basic.loc[df_basic["Type"] == "SELL"]
-
-###########################
-# Net Shares on Given Day #
-###########################
+# Get the shares held on a given day
 def getShareCount(day = pd.to_datetime("today")):
 
     # Get the buys and sells for each day before the given date
@@ -60,6 +37,46 @@ def getShareCount(day = pd.to_datetime("today")):
     netShares = netShares_all.loc[netShares_all["ShareCount"] != 0]
 
     return netShares
+
+
+# Get the value of a list of shares on a given date (E.O.D). 
+# Essentially just adds a 'value' column to the shares input df
+def getValue(shares, date):
+    print("Getting values of ")
+    print(shares)
+    print("On ")
+    print(date)
+    print("----------------") 
+
+#########
+# Setup #
+#########
+
+# Get a list of all tickers traded
+tickers_all = df_basic['Name'].unique()
+
+# Account for any stock splits
+allSplits = []
+for ticker in tickers_all:
+
+    # Get the split and convert to a df
+    splits = yf.Ticker(ticker).splits
+    df_split = pd.DataFrame({'Date':splits.index, 'Split':splits.values})
+    df_split["Name"] = ticker
+
+    # If there was a split, append the df to allSplits. The ignoring of PFE is a quirk with a spin off they did with Viatris in 2020. This is a bad fix but it'll do for now
+    if len(df_split.index) != 0 and ticker != "PFE":
+        allSplits.append(df_split)
+
+allSplits = pd.concat(allSplits)
+
+# Iterate through splits and alter share counts as necessary
+for index, row in allSplits.iterrows():
+    df_basic.loc[(df_basic["Name"] == row["Name"]) & (df_basic["Date"] < row["Date"]), ["ShareCount"]] = (df_basic['ShareCount'] * row["Split"]).round(6)
+
+#Split buys and sells into separate dataframes
+df_buys = df_basic.loc[df_basic["Type"] == "BUY"]
+df_sells = df_basic.loc[df_basic["Type"] == "SELL"]
 
 # A list of current tickers
 tickers_current = getShareCount().index.values.tolist()
@@ -101,5 +118,14 @@ df_dailySummary = pd.concat([df_dailySummary, df_basicPivot, df_diviPivot], axis
 df_dailySummary = df_dailySummary.rename(columns={"BUY": "Total Bought", "SELL": "Total Sold"})
 df_dailySummary = df_dailySummary
 df_dailySummary['Net Income']= df_dailySummary.sum(axis=1)
+df_dailySummary = df_dailySummary.reset_index()
 
-df_dailySummary.to_csv('./outputs/daily.csv', index=True)
+uniqueDays = df_dailySummary['Date'].unique()
+
+for day in uniqueDays:
+    getValue(getShareCount(day), day)
+    # Need to think more about uniquedays. Probably want to just get the earliest and interpolate then -> now.
+    # Will be slow af as all stocks will get all data from first date
+    # Maybe need a new lookup with ticker + first buy date, to limit number of api calls?
+
+df_dailySummary.to_csv('./outputs/daily.csv', index=False)
